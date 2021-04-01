c22f490820a660825a3ec3f46721f529752eecf4
Amonsta <amana.ammi@gmail.com>
2021-03-31 20:29:52 -0600

    add to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
-import './App.css';
+import React, {lazy, Suspense} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const Dashboard = lazy(() => import('./pages/dashboard'));
+const Login = lazy(() => import('./pages/login'));
+const SignUp = lazy(() => import('./pages/signup'));
+const Profile = lazy(() => import('./pages/profile'));
+const NOT_FOUND = lazy(() => import('./pages/not-found'));
