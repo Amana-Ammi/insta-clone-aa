c7c1717b347019320afd2421c0f31ac04bc6ddb1
Amonsta <amana.ammi@gmail.com>
2021-04-11 22:52:20 -0600

    add state to email and password

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -4,9 +4,12 @@ import * as ROUTES from '../constants/routes';
 
 export default function Login() {
 
-        useEffect(() => {
-            document.title = 'Login - Instagram';
-        }, [])
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+
+    useEffect(() => {
+        document.title = 'Login - Instagram';
+    }, [])
 
     return (
         <div className="container flex mx-auto max-w-screen-md items-center h-screen">
