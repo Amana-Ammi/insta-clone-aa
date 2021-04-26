2c3a18e4dafd3dd3f80774763d7623f5773279da
Amonsta <amana.ammi@gmail.com>
2021-04-25 21:34:43 -0600

    change to cursor not allowed

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -42,7 +42,7 @@ export default function Login() {
                     <button
                         disabled={isInvalid}
                         type="submit"
-                        className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${ isInvalid && 'cursor-none opacity-50'
+                        className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${ isInvalid && 'cursor-not-allowed opacity-50'
                         }`}
                     >
                         Log In
